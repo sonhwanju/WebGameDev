567b141a608693717b30f2ea790f902ee71b809b
hwanjuson <hwanjuson@gmail.com>
2021-04-06 20:21:26 +0900

    끝

--- myGame/js/App.js
+++ myGame/js/App.js
@@ -1,36 +1,75 @@
 class App 
 {
-    constructor() {
-        this.player = new Player(290,190);
+    constructor(){
+        this.player = new Player(290, 190);
         this.canvas = document.querySelector("#myGame");
         this.ctx = this.canvas.getContext("2d");
 
-        this.coin = new Coin(80,80,20,20);
-        this.coin2 = new Coin(160,160,20,20);
-        this.coin3 = new Coin(20,20,20,20);
-        
-                                         //=> 써야 위 참조가능
-        this.canvas.addEventListener("click", (e)=> {
-        this.player.setTarget(e.offsetX - this.player.w/2,e.offsetY - this.player.h/2);
+        this.coinList = []; //비어있는 배열 생성
+        for(let i = 0; i < 10; i++){
+            this.coinList.push(new Coin());
+        }
+
+        this.canvas.addEventListener("click", (e)=>{
+            this.player.setTarget(e.offsetX, e.offsetY);
         });
-       setInterval(()=>{
+
+        this.gameOver = false; //게임오버가 아닌상태
+
+        setInterval(()=> {
             this.update();
-            this.render(); 
+            this.render();
         }, 1000/60);
+        
     }
 
-    update() {
+    update(){
+        if(this.gameOver) return;
+
         this.player.update(1/60);
-        this.coin.update(1/60);
-        this.coin2.update(1/60);
-        this.coin3.update(1/60);
-    }
-    render() {
-        this.ctx.clearRect(0,0,600,400);
-        this.player.render(this.ctx);
-        this.coin.render(this.ctx);
-        this.coin2.render(this.ctx);
-        this.coin3.render(this.ctx);
+        
+        for(let i = 0; i < this.coinList.length; i++){
+            this.coinList[i].update(1/60);
+            if(this.coinList[i].checkCol(this.player)){
+                //충돌시에 this.coinList[i]가 isRed 가 true
+                if(this.coinList[i].isRed){
+                    this.setGameOver();//게임오버함수 실행
+                }else{
+                    this.coinList[i].remove();//코인제거
+                }
+            }
+        }
+
+        // let coinCnt = 0;
+        // for(let i = 0; i < this.coinList.length; i++){
+        //     if(this.coinList[i].active){
+        //         coinCnt++;
+        //     }
+        // }
+        let coinCnt = this.coinList.filter(x => x.active).length;
+
+        if(coinCnt == 0){
+            this.setGameClear();
+        }
+
     }
 
+    setGameOver(){
+        alert("게임오버");
+        this.gameOver = true;
+    }
+    setGameClear(){
+        alert("게임 클리어");
+    }
+
+    render(){
+        this.ctx.clearRect(0,0,600, 400);
+        this.player.render(this.ctx);
+        for(let i = 0; i < this.coinList.length; i++){
+            this.coinList[i].render(this.ctx);
+        }
+    }
+    
 }
+
+                                
--- myGame/js/Coin.js
+++ myGame/js/Coin.js
@@ -13,15 +13,20 @@ class Coin
         this.t = 0;
         this.nextT = 3;
         this.isRed = false;
-
+        this.active = true;
         this.reset();
+    }
+    remove() {
+        this.active = false;
     }
     reset() {
         this.w = this.h = Math.random()*30 + 10;
         this.x = Math.random()*(600-this.w);
         this.y = Math.random()*(400-this.h);
+        this.nextT = Math.random() * 4 + 1;
     }
     update(d) {
+        if(!this.active) return;
         this.t += d;
         if(this.t >= this.nextT) {
             this.t = 0;
@@ -31,13 +36,26 @@ class Coin
     }
 
     render(ctx) {
-        if(this.isRed == true) {
+        if(!this.active) return;
+        if(this.isRed) {
             ctx.drawImage(this.redCoinSprite,this.x,this.y,this.w,this.h);
         }
         else if(this.isRed == false){
             ctx.drawImage(this.coinSprite,this.x,this.y,this.w,this.h);
         }
         
+    }
+
+    checkCol(p){
+        if(!this.active) return false;
+        // 두 물체의 중심점간의 거리가, 두 물체의 반지름의 합보다
+        //작으면 충돌이야.
+        let pr = p.w / 2;
+        let dx = (p.x + pr) - (this.x + this.w/2);
+        let dy = (p.y + pr) - (this.y + this.h/2);
+        let d = Math.sqrt( dx * dx + dy * dy); //두점사이의 거리
+
+        return d <= pr + this.w/2 ;
     }
 
    
