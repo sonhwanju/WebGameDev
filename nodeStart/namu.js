07ae04e3553a45efd11da8eda63c933ec46ef830
hwanjuson <hwanjuson@gmail.com>
2021-04-12 21:23:48 +0900

    숙제 끝

--- nodeStart/namu.js
+++ nodeStart/namu.js
@@ -1,18 +1,38 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
 let titleId = process.argv[2];
-let no = process.argv[3];
+let no = process.argv[3]*1;
+let no2 = process.argv[4]*1;
 
 
-let uri = `https://comic.naver.com/webtoon/detail.nhn?titleId=${titleId}&no=${no}`;
+for(let j = no; j <= no2; j++) {
+    let uri = `https://comic.naver.com/webtoon/detail.nhn?titleId=${titleId}&no=${j}`;
+    request(uri, function(err,res,body) {
 
-request(uri, function(err,res,body) {
-    let $ = cheerio.load(body);
-    let list = $(".wt_viewer > img");
-    for(let i = 0; i < list.length;i++) {
-        let src = list.eq(i).attr("src");
-        console.log(src);
-    }
     
-});
+        let $ = cheerio.load(body);
+        let list = $(".wt_viewer > img");
+    
+        fs.mkdirSync(titleId + "/" + j, {recursive:true});
+        for(let i = 0; i < list.length;i++) {
+            let src = list.eq(i).attr("src");
+            download(src,j + "/" + `${i}.jpg`);
+        }
+        
+    });
+}
+
+
+function download(src,filename) {
+    let option = {
+        method:"GET", // POST, PUT, DELETE
+        uri:src,
+        headers:{"User-Agent":"Mozilla/5.0"},
+        encoding:null
+    };
+    
+    request(option).pipe(fs.createWriteStream(titleId + "/" + filename));
+
+}
