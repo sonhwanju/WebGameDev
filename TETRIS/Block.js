7674ad2bc44cbf9d1f4fb024a3df05c145cbf1ae
hwanjuson <hwanjuson@gmail.com>
2021-05-03 22:07:37 +0900

    ㅁㄴㄹ

--- TETRIS/Block.js
+++ TETRIS/Block.js
@@ -1,13 +1,29 @@
-class Block{
+export class Block{
     constructor(x,y) {
         this.x = x;
         this.y = y;
+        this.size = 35;
+        this.padding = 2;
+        this.fill = false;
+        this.color = "#ff0000";
+    }
+
+    setBloackData(fill, color) {
+        this.fill = fill;
+        this.color = color;
     }
 
     update(delta) {
 
     }
     render(ctx) {
-        
+        //사각형 그리는 애 x y width height
+        ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+        ctx.strokeRect(this.x*this.size+this.padding, this.y*this.size+this.padding, this.size - 1, this.size - 1);
+
+        if(this.fill) {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x*this.size+this.padding, this.y*this.size+this.padding, this.size - 1, this.size - 1);
+        }
     }
 }
--- TETRIS/Game.js
+++ TETRIS/Game.js
@@ -1,3 +1,5 @@
+import {Block} from '/TETRIS/Block.js';
+
 export class Game {
     constructor() {
         this.canvas = document.querySelector("#gameCanvas");
@@ -7,10 +9,12 @@ export class Game {
     }
 
     update(){
-
+        this.arr.forEach(row=> row.forEach(item=>item.update(1000/30)));
     }
     render(){
+        this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
 
+        this.arr.forEach(row=> row.forEach(item=>item.render(this.ctx)));
     }
 
     start() {
@@ -20,6 +24,22 @@ export class Game {
         this.frame = setInterval(()=> {
             this.update();
             this.render();
-        });
+        },1000/30);
+        this.arr = [];
+        for(let i = 0; i < 20; i++) {
+            let row = []
+            for(let j = 0; j < 10;j++) {
+                row.push(new Block(j,i));
+            }
+            this.arr.push(row);
+        }
+        //this.debug();
+    }
+    debug(){
+        this.arr[19][0].setBloackData(true, "#007bff");
+        this.arr[19][1].setBloackData(true, "#007bff");
+        this.arr[19][2].setBloackData(true, "#007bff");
+        this.arr[19][3].setBloackData(true, "#007bff");
+
     }
 }
