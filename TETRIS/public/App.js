bfe13e23e76a7853a445570735edde8839445e22
hwanjuson <hwanjuson@gmail.com>
2021-05-12 10:47:17 +0900

    2

--- TETRIS/public/App.js
+++ TETRIS/public/App.js
@@ -3,6 +3,7 @@ import {Game} from '/Game.js';
 class App {
     constructor() {
         this.socket = new io();
+        this.addSocketEvent();
         this.pageContainer = $(".page-container");
         this.init();
         //this.debug(); //테스트용코드
@@ -11,8 +12,16 @@ class App {
     }
     init() {
         $("#btnLogin").addEventListener("click", ()=> {
-            let pc = document.querySelector(".page-container");
-            pc.style.left = "-2048px";
+            // let pc = document.querySelector(".page-container");
+            // pc.style.left = "-2048px";
+            let email = $("#loginEmail").value;
+            let pw = $("#loginPassword").value;
+
+            if(email.trim() === "" || pw.trim() === "") {
+                alert("필수값이 비어있습니다.")
+                return;
+            }
+            this.socket.emit("login-process",{email,pw});
 
         });
         $("#btnStart").addEventListener("click",()=>{
@@ -28,13 +37,35 @@ class App {
         });
     }
 
+    addSocketEvent() {
+        this.socket.on("register-response",data=> {
+            alert(data.msg);
+
+            if(data.status) {
+                $("#registerEmail").value = "";
+                $("#registerName").value = "";
+                $("#registerPass").value = "";
+                $("#registerPassConfirm").value = "";
+            }
+        });
+        this.socket.on("login-response", data=> {
+            alert(data.msg);
+            if(data.status) {
+                $("#loginEmail").value = "";
+                $("#loginPassword").value = "";
+                this.pageContainer.style.left = "-1024px";
+            }
+        });
+        
+    }
+
     registerProcess() {
         let email = $("#registerEmail").value;
         let name = $("#registerName").value;
         let pw = $("#registerPass").value;
-        let pwx = $("#registerPassConfirm").value;
+        let pwc = $("#registerPassConfirm").value;
 
-        if(email.trim() === "" || name.trim()==="" || pw.trim() === "" || pw!==pwc) {
+        if(email.trim() === "" || name.trim()==="" || pw.trim() === "" || pw !==pwc) {
             alert("공백이거나 비밀번호 확인이 일치하지 않습니다.");
             return;
         }
--- TETRIS/server.js
+++ TETRIS/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
+const {Query,Pool} = require('./DB.js');
 
 const app = new express();
 const server = http.createServer(app);
@@ -10,19 +11,49 @@ const io = socketio(server);
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-    res.sendfile(path.join(__dirname, "views", "index.html"));
+    res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 io.on("connect", socket=> {
     console.log(`socket connected : ${socket.id}`);
 
-    socket.on("register-request", data=> {
+    socket.on("login-process", async data=> {
+        const {email,pw} = data;
+        let sql = "SELECT * FROM users WHERE email = ? AND password = PASSWORD(?)";
+        let result = await Query(sql, [email,pw]);
+        if(result.length !==1) {
+            socket.emit("login-response", {status:false,msg:"로그인 실패"});
+            return;
+        }
+        socket.emit("login-response", {status:true, msg:"로그인 성공"});
+        socket.loginUser = result[0]; //로그인된 유저를 socket 데이터에 넣어준다.
+    });
+
+    socket.on("register-request", async data=> {
         const {email,name,pw,pwc} = data;
 
         if(email.trim() === "" || name.trim()==="" || pw.trim() === "" || pw!==pwc) {
             socket.emit("register-response", {status:false,msg:"비어있거나 비밀번호 확인이 올바르지 않습니다."});
             return;
         }
+
+        let sql = "SELECT * FROM users WHERE email = ?";
+
+        let result = await Query(sql, [email]);
+        if(result.length != 0) {
+            socket.emit("register-response", {status:false,msg:"이미 존재하는 회원입니다."});
+            return;
+        }
+
+        sql = `INSERT INTO users(email, name, password) VALUES (?, ?, PASSWORD(?))`;
+        result = await Query(sql, [email, name, pw]);
+
+        if(result.affectedRows == 1) {
+            socket.emit("register-response", {status:true,msg:"정상적으로 회원가입 되었습니다."});
+        }else {
+            socket.emit("register-response", {status:false,msg:"데이터베이스 처리중 오류 발생"});
+        }
+        return;
     });
 });
 
